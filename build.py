a04555972526d90f49473f15e3f50be1de811d74
Andrea Dainese <andrea.dainese@pm.me>
2025-09-22 11:48:17 +0200

    update

--- build.py
+++ build.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-import json
 import yaml
 from jinja2 import Template
 
 main_template_file = "templates/index.j2.html"
 main_file = "index.html"
-episodes_data_file = "config/episodes.json"
+episodes_data_file = "config/episodes.yaml"
 platforms_data_tile = "config/platforms.yaml"
 
 with open(main_template_file, "r", encoding="utf-8") as fh:
     main_template_html = fh.read()
 with open(episodes_data_file, "r", encoding="utf-8") as fh:
-    episodes = json.load(fh)
+    episodes = yaml.safe_load(fh)
     episodes.reverse()
 with open(platforms_data_tile, "r", encoding="utf-8") as fh:
     platforms = yaml.safe_load(fh)
